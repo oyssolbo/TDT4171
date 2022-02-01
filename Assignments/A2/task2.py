0a86af8665f3d7296487e2a657efdea34bbf64a7
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-02-01 08:20:48 +0100

    Solved task 2
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- Assignments/A2/task2.py
+++ Assignments/A2/task2.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 from numpy import ndarray
 
 # Using the models:
@@ -9,11 +11,13 @@ F = np.array(
     [0.7, 0.3],
     [0.3, 0.7]
   ]) 
-H = np.array(
+H_given_rain = np.array(
   [
     [0.9, 0],
     [0,   0.2]
   ])
+H_given_no_rain = 1 - H_given_rain
+H = np.array([H_given_no_rain, H_given_rain])
 
 # Initial value
 x0 = np.array([[0.5], [0.5]])
@@ -21,9 +25,7 @@ x0 = np.array([[0.5], [0.5]])
 # Observations
 z = np.array([1, 1, 0, 1, 1])
 
-# Just a cleaner version, while maintaining the comments/discussions I made
-# above. SLightly modified to be more efficient and use DP
-def forward(curr_idx: 'int', known_idx=0, known_value=x0)->float:
+def forward(curr_idx: 'int', known_idx=0, known_value=x0)->ndarray:
   if curr_idx == 0:
     return x0
 
@@ -32,15 +34,11 @@ def forward(curr_idx: 'int', known_idx=0, known_value=x0)->float:
   else:
     sum = F @ forward(curr_idx - 1)
   
-  prod = H @ sum
+  O = H[z[idx]]
+  prod = O @ sum
   return prod / prod.sum(axis=0)  
 
 if __name__ == '__main__':
-  # I have the problem that I don't use the values for z at all
-  # This means that it will converge eventually, independent on the 
-  # measurements for z
-
-  # Looks like it just converges onto 0.9, since the values for z are not used
   xf = np.zeros((z.shape[0], 2))
   for idx, x in enumerate(xf):
     if idx == 0:
@@ -49,33 +47,14 @@ if __name__ == '__main__':
       known_idx = idx - 1
     xf[idx,:] = forward(idx, known_idx, xf[known_idx]).T
 
+  plt.scatter(list(range(z.shape[0])), xf[:,0])
+  plt.ylim([0.0, 1.0])
+  plt.title("Probability for rain on a given day")
+  plt.xlabel("Day")
+  plt.ylabel("P(x | z)")
+  plt.show()
+
+  # For just getting the raw data
   print(xf)
 
 
-
-
-
-# def forward(curr_idx: 'int', known_idx=0, known_value=x0)->float:
-#   if curr_idx == 0:
-#     return x0
-  
-#   # But this is incorrect. I cannot understand how one should determine the
-#   # value for H, since it will vary depending on the state of x. Since x is
-#   # unknown, I cannot see how one should calulcate/determine this matrix. It 
-#   # is wrong to just use the value for z, as it does not imply which  
-#   if z[curr_idx] == 1:
-#     O = H
-#   else:
-#     # And as expected, one gets a totally different result when using 1 - H
-#     # as one should, as it is not correct
-#     O = H #(1 - H)
-
-#   # Initializing memory
-#   sum = F @ forward(curr_idx - 1) #np.zeros((2, 1))
-
-#   # Summing over array using recursion
-#   # sum = F @ forward(idx - k - 1) #?
-#   # for k in range(idx):
-#   #   sum = sum + F @ forward(idx - k - 1)
-#   prod = O @ sum
-#   return prod / prod.sum(axis=0)  
