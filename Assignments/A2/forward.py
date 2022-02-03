38b2095e0b0aa5dbfdd78c16a40adb61d2270b45
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-02-03 12:20:33 +0100

    Slight bugfix for forward-pass
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- Assignments/A2/forward.py
+++ Assignments/A2/forward.py
@@ -4,15 +4,19 @@ import matplotlib.pyplot as plt
 from numpy import ndarray
 from parameters import UmbrellaWorldParameters as uw_param
 
-def forward(curr_idx: 'int', known_idx=0, known_value=uw_param.x0)->ndarray:
+def forward(curr_idx: 'int', min_idx=0, known_idx=None, known_value=None)->ndarray:
+  if curr_idx >= uw_param.z.shape[0] or curr_idx < min_idx:
+    return np.zeros_like(uw_param.x0)
+  
   O = uw_param.H[uw_param.z[curr_idx]]
   OFT = O @ uw_param.F.T
 
-  if curr_idx == 0:
+  if curr_idx == min_idx:
     prod = OFT @ uw_param.x0
     return prod / prod.sum(axis=0)
 
-  if known_idx != 0 and curr_idx == known_idx:
+  # Start on DP
+  if known_idx is not None and curr_idx is not None:
     val = known_value
   else:
     val = forward(curr_idx - 1)
@@ -27,7 +31,7 @@ if __name__ == '__main__':
       known_idx = 0
     else:
       known_idx = idx - 1
-    xf[idx,:] = forward(idx, known_idx, xf[known_idx]).T
+    xf[idx,:] = forward(idx, 0, known_idx, xf[known_idx]).T
 
   plt.scatter(list(range(uw_param.z.shape[0])), xf[:,0])
   plt.ylim([0.0, 1.0])
