b8d52e37ab575a129c6b1cd8d34fd6ad9518fcaf
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-02-03 12:20:59 +0100

    Slight failsafe for backwards pass
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- Assignments/A2/backward.py
+++ Assignments/A2/backward.py
@@ -6,20 +6,17 @@ from parameters import UmbrellaWorldParameters as uw_param
 
 
 def backward(curr_idx: 'int', max_idx: 'int')->ndarray:
-  # Unsure whether this should be normalized or not
-  # Equation 14.13 in the book does not use normalization, however
-  # it does not make sence to give probability that does not sum 
-  # up to 1. I therefore normalize here 
+  if curr_idx > max_idx or curr_idx < 0:
+    return np.ones_like(uw_param.x0)
 
   O = uw_param.H[uw_param.z[curr_idx]]
   FO = uw_param.F @ O
 
+  if curr_idx > max_idx:
+    return np.ones_like(uw_param.x0)
   if curr_idx == max_idx:
-    prod = FO @ np.ones_like(uw_param.x0)
-    return prod / prod.sum() 
-  prod = FO @ backward(curr_idx + 1, max_idx)
-  return prod / prod.sum()
-
+    return FO @ np.ones_like(uw_param.x0)
+  return FO @ backward(curr_idx + 1, max_idx)
 
 if __name__ == '__main__':
   xb = np.ones((uw_param.z.shape[0], 2))
@@ -28,9 +25,9 @@ if __name__ == '__main__':
 
   plt.scatter(list(range(uw_param.z.shape[0])), xb[:,0])
   plt.ylim([0.0, 1.0])
-  plt.title("Backward probability (normalized)")
+  plt.title("Backward probability (not normalized)")
   plt.xlabel("Start-date of the backwards propagation")
-  plt.ylabel("P(z_(t:T) | x_t)")
+  plt.ylabel("P(z_(k + 1:T) | x_k)")
   plt.show()
 
   # For just getting the raw data
