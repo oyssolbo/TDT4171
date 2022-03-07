5ef14c8eaa0dda1ff190b7f2dae3bcc350438e07
Øystein Solbø <oyssolbo@stud.ntnu.no>
2022-03-07 19:20:57 +0100

    Trying to add the importance function
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- Assignments/A4/importance.py
+++ Assignments/A4/importance.py
@@ -4,16 +4,13 @@ import random
 import warnings
 
 class Importance:
-  # def random(
-  #     self,
-  #     attribute_array : np.ndarray
-  #   ) -> int:
-  #   """
-  #   Randomly selects an attribute to separate on  
-  #   """
-  #   attribute_array = attribute_array.reshape((1,-1))
-  #   assert attribute_array.shape[0] > 0 and attribute_array.shape[1] > 0
-  #   return random.randint(0, attribute_array.shape[1] - 1)
+  def __init__(
+        self,
+        min_val : int,
+        max_val : int
+      ) -> None:
+    self.min_val = min_val
+    self.max_val = max_val
 
   def random(
       self,
@@ -40,47 +37,52 @@ class Importance:
 
     def binary_entropy(probability : float) -> float:
       assert probability >= 0 and probability <= 1, "Probability out of range"
-      if probability <= 1e-12: return 0
+      if probability <= 1e-12: return 0 # To prevent problems with lb(0)
       return -(probability * np.log2(probability) + (1 - probability) * np.log2(1 - probability))
 
-    # Extracting total number of 'positive' and 'negative'    
-    # Using prior knowledge that the values will only be 1 or 2
-    min_val = 1
-    max_val = 2
+    assert isinstance(data_node_list, list), "Training data must be given as a list"
+    assert len(data_node_list) > 0, "No data received"
 
-    num_min_vals = 0
-    num_max_vals = 0
+    # Iterate through all of the columns and extract number of 1 and 2 for each columnn
+    num_vals_per_column = np.zeros((self.max_val - self.min_val + 1, data_node_list[0].get_data().reshape((1,-1)).shape[1]), dtype=int)
 
     for node in data_node_list:
-      data_type = node.get_type()
-      if data_type == min_val:
-        num_min_vals += 1
-      elif data_type == max_val:
-        num_max_vals += 1
-      else:
-        warnings.warn("Other value detected: {}".format(data_type))
+      node_data = node.get_data()
+      for (attribute, data) in enumerate(node_data):
+        for val in range(self.min_val, self.max_val + 1):
+          if val == data:
+            num_vals_per_column[val - self.min_val, attribute] += 1
 
-    # Iterating through all nodes, and determining the optimal attribute
-    optimal_attribute = 0
-    optimal_attribute_num_pos = 0
-    # for node in 
-    # for val in range(min_val, max_val + 1):
+    # Finding total amount of values
+    # total_num_min_vals = np.sum(num_vals_per_column[0])
+    # total_num_max_vals = np.sum(num_vals_per_column[1])
 
-    # Understand how to caculate the remainder
+    # Calculating total binary entropy
+    # total_bin_entropy = binary_entropy(probability=(num_vals_per_column[self.min_val] / len(data_node_list)))
 
-    # Calculate the information-gain
+    #binary_entropy(probability=(total_num_min_vals / (total_num_min_vals + total_num_max_vals)))
 
-    # Select the attribute which maximizes the information gain
+    # Using entropy to calculate the optimal entropy
+    gain_arr = -np.ones((1, num_vals_per_column.shape[1]), dtype=float).reshape((1,-1))
 
-    return 0
+    for attribute in range(num_vals_per_column.shape[1]):
+      # Calculate binary entropy for a given attribute
+      bin_entropy = binary_entropy(probability=(num_vals_per_column[self.min_val, attribute] / len(data_node_list)))
 
-  def remainder(self):
-    return 0
+      # Calculate information gain
+      num_mins = num_vals_per_column[0, attribute]
+      num_max = num_vals_per_column[1, attribute]
 
+      gain = 0
 
-  def information_gain(self):
-    return 0
+      # Thinks that the problem is here somewhere. As written in the book:
+      # Each subset Ek has pk positive examples and nk negative examples. Would this mean that these lines must be changed?
+      attribute_probability = (num_mins + num_max) / len(data_node_list) #(total_num_min_vals + total_num_max_vals)
+      binary_probability = num_mins / (num_mins + num_max)
+      gain_arr[0, attribute] = bin_entropy - (attribute_probability * binary_entropy(probability=binary_probability))
 
-  
+    # Calculating the information gain. Return attribute which maxes this
+    # gain_arr = 0# total_bin_entropy - remainder_arr
+    print(np.argmax(gain_arr))
 
-
+    return np.argmax(gain_arr)
